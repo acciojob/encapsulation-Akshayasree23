011cb3a1014db5770984e2a5958c327ceffc9c55
akshaya <akshayasri233@gmail.com>
2025-01-23 23:24:12 +0530

    solved Encapsulation Assignment

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,13 @@
 package com.driver;
 
 public class Main {
-  
+
+    public static void main(String[] args) {
+
+        RWOnly rwonly = new RWOnly();
+        rwonly.setName("Akki");
+        //name' has private access in 'com. driver. RWOnly'
+        //System.out.println(rwonly.name);
+        System.out.println(rwonly.getName());
+    }
 }
--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,18 @@
 package com.driver;
 
+import org.w3c.dom.ls.LSOutput;
+
+import java.sql.SQLOutput;
+
 public class RWOnly {
-    
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
